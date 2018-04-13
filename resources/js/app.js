ebe0f803dece0ad7a22f18dcd7c4cecf55861ac8
Kevin Wong <kvn.m.wong@gmail.com>
2018-04-13 15:00:07 -0700

    creates FilterSetup.js component - removes FilterSetup class fro app.js - imports component into index.html

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,26 +1,3 @@
-class FilterSetup extends React.Component {
-    constructor(props) {
-        super(props);
-        this.updateFilter = this.updateFilter.bind(this);
-    }
-    updateFilter(filter, value) {
-        this.props.handleChange(filter, value);
-    }
-    render() {
-        const filters = this.props.filters;
-        return (
-            <div className="form-group form-inline">
-                <FilterCheckbox label="Show only carts" name="cartsOnly" value={filters.cartsOnly} handleChange={this.updateFilter} />
-                <FilterCheckbox label="Main square only" name="mainSquare" value={filters.mainSquare} handleChange={this.updateFilter} />
-                <strong className="mr-2">Dietary options:</strong>
-                <FilterCheckbox label="Vegetarian" name="veggie" value={filters.veggie} handleChange={this.updateFilter} />
-                <FilterCheckbox label="Vegan" name="vegan" value={filters.vegan} handleChange={this.updateFilter} />
-                <FilterCheckbox label="Gluten-free" name="gf" value={filters.gf} handleChange={this.updateFilter} />
-            </div>
-        );
-    }
-}
-
 class App extends React.Component {
     constructor(props) {
         super(props);
