e842d8f1abdfa3e29143c8989cfb2e275e172e5e
Kevin Wong <kvn.m.wong@gmail.com>
2018-05-04 16:08:40 -0700

    changes class attrs to className

--- resources/js/components/Footer.js
+++ resources/js/components/Footer.js
@@ -1,8 +1,8 @@
 function Footer(){
     return (
         <footer>
-            <article class="container border pt-3 mb-4">
-                <p class="alert alert-warning"><strong>Disclaimer</strong>: If you have any allergies or gluten intolerance, we advise precaution. Please speak with the staff of an establishment before ordering to ensure it’s made to suit your needs.</p>
+            <article className="container border pt-3 mb-4">
+                <p className="alert alert-warning"><strong>Disclaimer</strong>: If you have any allergies or gluten intolerance, we advise precaution. Please speak with the staff of an establishment before ordering to ensure it’s made to suit your needs.</p>
             </article>
         </footer>
     );
--- resources/js/components/Header.js
+++ resources/js/components/Header.js
@@ -1,10 +1,10 @@
 function Header(){
     return (
         <header>
-            <div class="container pt-4 pb-4">
-                <h1 id="app-title" class="text-center">PDX Food Cart Recs</h1>
+            <div className="container pt-4 pb-4">
+                <h1 id="app-title" className="text-center">PDX Food Cart Recs</h1>
             </div>
-            <article class="container border pt-3 mb-4">
+            <article className="container border pt-3 mb-4">
                 <p>These are some of our favorite meals from some of our favorite places to eat. The meal we suggest might not fit your dietary needs, but we recommend looking at the full menu if something piques your interest.</p>
             </article>
         </header>
