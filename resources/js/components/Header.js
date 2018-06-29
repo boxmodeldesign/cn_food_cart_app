a2a3ea0e3e0a006252b4651e76336c8b9f28d111
Brant Hajek <branthajek@gmail.com>
2018-06-29 13:34:11 -0700

    Title update

--- resources/js/components/Header.js
+++ resources/js/components/Header.js
@@ -2,7 +2,7 @@ function Header(){
     return (
         <header>
             <div className="container pt-4 pb-4">
-                <h1 id="app-title">PDX Food Cart Recs</h1>
+                <h1 id="app-title">Highly Opinionated Food Recommendations</h1>
             </div>
             <article className="container mb-4">
                 <p className="alert alert-info">
